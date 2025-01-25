42aa746025426834942de40657a755fcad08e527
Serigne Bassirou Ndiaye <dstndiaye@gmail.com>
2025-01-25 10:03:52 +0100

    page home

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Set page config
+st.set_page_config(
+    page_title="Tableau de Bord Temps reel",
+    page_icon=None,
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
